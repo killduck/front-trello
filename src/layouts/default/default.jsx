2792fff05838d3a7288d37e862376314f6a06c34
Ilya616 <ipoletuev@mail.ru>
2024-05-03 21:15:11 +0300

    structure header

---
+++ src/layouts/default/default.jsx
@@ -0,0 +1,21 @@
+import Search from "../../components/Search/Search";
+
+export default function Default(props) {
+  return (
+    <div>
+      <header>
+
+        <nav></nav>
+
+        <Search />
+
+        <div>
+          уведомления
+          информация
+          аккаунт
+        </div>
+
+      </header>
+    </div>
+  )
+};
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Default from "../../layouts/default/default";
+
+export default function Dashboard(props){
+    
+  return (
+    <div>
+      <Default>
+        контент страницы
+      </Default>
+    </div>
+  )
+};
