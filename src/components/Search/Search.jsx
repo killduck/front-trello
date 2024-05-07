a7b66ad233bf96ef892646e5d64c44dc450472b6
Leo_Kildushev <kildushev@gmail.com>
2024-05-07 13:48:55 +0300

    сделал элемент "input": вёрстка+ стили,но без функционала

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,12 @@
 import Button from '../ui/Button/Button';
+import InputSearch from '../ui/InputSearch/InputSearch';
 import styles from './Search.module.scss';
 
 export default function Search(props) {
     return (
         <div className={styles.Search}>
 
-
+            <InputSearch/>
             <Button>Поиск</Button>
         </div>
     )
