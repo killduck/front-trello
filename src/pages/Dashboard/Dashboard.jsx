36d4c12b21d4ca7143f6c4537f7e9fd0b05d64a3
Ilya616 <ipoletuev@mail.ru>
2024-05-24 20:45:02 +0300

    test lib drag and drop

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,10 +5,121 @@ import Default from "../../layouts/default/Default";
 import styles from "./Dashboard.module.scss";
 import axios from "axios";
 
+// import { Droppable } from './Droppable';
+// import { Draggable } from './Draggable';
+
 import { useState, useEffect } from "react";
 
 
+import { closestCenter, DndContext } from "@dnd-kit/core";
+
+import { CSS } from "@dnd-kit/utilities";
+
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+const data = [
+  {
+    id: 1,
+    name: "Samaria",
+  },
+  {
+    id: 2,
+    name: "Gauthier",
+  },
+  {
+    id: 3,
+    name: "Mellisa",
+  },
+  {
+    id: 4,
+    name: "Arabela",
+  },
+  {
+    id: 5,
+    name: "Devon",
+  },
+  {
+    id: 6,
+    name: "Stacee",
+  },
+  {
+    id: 7,
+    name: "Federica",
+  },
+  {
+    id: 8,
+    name: "Jecho",
+  },
+  {
+    id: 9,
+    name: "Alasteir",
+  },
+  {
+    id: 10,
+    name: "Elston",
+  },
+];
+
+
+const SortableUser = ({ user }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: user.id });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="user"
+    >
+      {user.name}
+    </div>
+  );
+};
+
+
+
+
 export default function Dashboard(props) {
+
+  const [users, setUsers] = useState(data);
+  const [inputValue, setInputValue] = useState("");
+  const addUser = () => {
+    let newUser = {
+      id: "1",
+      name: inputValue,
+    };
+    setInputValue("");
+    setUsers((users) => [...users, newUser]);
+  };
+
+  const onDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setUsers((users) => {
+      const oldIndex = users.findIndex((user) => user.id === active.id);
+      const newIndex = users.findIndex((user) => user.id === over.id);
+      return arrayMove(users, oldIndex, newIndex);
+    });
+  };
+
 
   const [columns, setColumns] = useState([]);
   const [show, setShowElement] = useState(true);
@@ -44,6 +155,18 @@ export default function Dashboard(props) {
   return (
     <div>
       <Default>
+
+        <div>
+          <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+            <SortableContext items={users} strategy={verticalListSortingStrategy}>
+              {users.map((user) => (
+                <SortableUser key={user.id} user={user} />
+              ))}
+            </SortableContext>
+          </DndContext>
+        </div>
+
+
         <div className={styles.Columns}>
           {
             columns.map((column) =>
