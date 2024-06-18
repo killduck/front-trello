586f7056d38bccf4061928a3958e16611531935b
Leo_Kildushev <kildushev@gmail.com>
2024-06-18 14:59:55 +0300

    переисал css на scss для Auth.jsx

--- src/components/ui/Icons/Icons.jsx
+++ src/components/ui/Icons/Icons.jsx
@@ -1,10 +1,17 @@
 import IconsSVG from './icons.svg';
 import styles from './Icons.module.scss'
 
-export default function Icons({ name, class_name, active_icon, color, sizeLine, sizeWidth, sizeHeight, className}) {
-
+export default function Icons({ 
+  name, 
+  class_name, 
+  active_icon, 
+  color, sizeLine, 
+  sizeWidth, 
+  sizeHeight, 
+  className // нужно разобраться зачем тут className ???
+}) {
   return (
-    <svg className={class_name? `${styles.Icons} ${className} ${styles[class_name]} ${styles[active_icon]}` : ''} fill={color} stroke={color} strokeWidth={sizeLine} width={sizeWidth} height={sizeHeight}>
+    <svg className={`${styles.Icons} ${className} ${styles[class_name]} ${styles[active_icon]}`} fill={color} stroke={color} strokeWidth={sizeLine} width={sizeWidth} height={sizeHeight}>
       <use xlinkHref={`${IconsSVG}#${name}`} />
     </svg>
   )
