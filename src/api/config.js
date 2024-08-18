19b608deac8e6e74d4f04f59c0053e835a8d3a0d
Andrey Rubtsov <aa.rubtsov@icloud.com>
2024-08-19 01:00:30 +0300

    edit SSL certificate

--- src/api/config.js
+++ src/api/config.js
@@ -4,11 +4,11 @@ export let host_name = window.location.hostname
 export const URL_API =
   host_name === 'top-python31.ru' || host_name === '147.45.185.171'
     ? 'https://147.45.185.171/'
-    : 'https://127.0.0.1:8000/'
+    : 'http://127.0.0.1:8000/'
 
 export const URL_PUBLIC =
   host_name === 'top-python31.ru' || host_name === '147.45.185.171'
     ? 'https://top-python31.ru/'
-    : 'https://127.0.0.1:3000/'
+    : 'http://127.0.0.1:3000/'
 
 export const URL_ENDPOINT = 'api/'
