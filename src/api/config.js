2b9d89696ae678d406657d8e4203ee8adfd8f045
Andrey Rubtsov <90990985+Raa78@users.noreply.github.com>
2024-08-19 10:20:50 +0300

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ export let host_name = window.location.hostname
 // Условия прописаны для удобства деплоя, что бы URL-ы не переписывать на реальные хосты сервера
 export const URL_API =
   host_name === 'top-python31.ru' || host_name === '147.45.185.171'
-    ? 'https://147.45.185.171/'
+    ? 'https://top-python31.ru/'
     : 'http://127.0.0.1:8000/'
 
 export const URL_PUBLIC =
