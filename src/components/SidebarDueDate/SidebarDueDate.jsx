85babe686c65ce7572cbb89a7a28ca2a571b6ea5
Leo_Kildushev <kildushev@gmail.com>
2024-08-25 02:32:52 +0300

    правка дизайна и функционала для dueDate

--- src/components/SidebarDueDate/SidebarDueDate.jsx
+++ src/components/SidebarDueDate/SidebarDueDate.jsx
@@ -15,26 +15,32 @@ export default function SidebarDueDate(props){
   let funcDueDateWindow = props.funcDueDateWindow; 
   let dueDateWindow = props.dueDateWindow; 
 
-  const [checkbox, setCheckbox] = useState(false);
+  const [checkbox, setCheckbox] = useState(true);
 
-  const [date, setDate] = useState(new Date());
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  // const [date, setDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(new Date());
+  // const [endDate, setEndDate] = useState('');
+  function takeDate(){
+    let month = startDate.getMonth() + 1;
+    let year = startDate.getFullYear();
+    let date = startDate.getDate();
+    return `${date}.${month}.${year}`
+  }
+  
 
-  // const pastDate = (date) => new Date() < date-1;
-
-  const handleChange = (range) => {
-    const [startDate, endDate] = range;
+  const handleChange = (date) => {
+    // const [startDate, endDate] = range;
     setCheckbox(true);
-    setStartDate(startDate);
-    setEndDate(endDate);
+    setStartDate(date);
+    // setEndDate(endDate);
   };
   
   registerLocale('ru', ru);
 
   function funcEraseDates(){
-    setStartDate('');
-    setEndDate('');
+    setStartDate(new Date());
+    // setEndDate('');
+    setCheckbox(false);
   }
 
   function onDeteWindow(){
@@ -77,49 +83,83 @@ export default function SidebarDueDate(props){
           {/* SidebarDueDate */}
           <DatePicker
             inline
-            style={{}}
             className={styles.DatePicker}
             selected={startDate}
-            onChange={handleChange}
+            onChange={(date) => handleChange(date)}
             startDate={startDate}
-            endDate={endDate}
-            selectsRange
+            minDate={new Date()}
+            // endDate={endDate}
+            // selectsRange
             // showTimeSelect
             showTimeInput
-           
-            autoFocus={true}
-            shouldCloseOnSelect={false}
+            // autoFocus={true}
+            // shouldCloseOnSelect={false}
             locale='ru'
-            dateFormat="d.m.yyyy"
-            minDate={new Date(1)}
+            
+            timeInputLabel=""
+            dateFormat="d.m.yyyy h:mm aa"
+            // timeFormat="HH:mm"
+            // todayButton="Сегодня"
 
-          />
+          >
+            <div className={styles.dueDateItemWrap}>
+              <label className={styles.dueDateItemTittle}>
+                Срок
+              </label>
+              <label className={styles.dueDateItem}>
+                <input className={styles.dueDateItemInput} type="checkbox" />
+                
+                <span className={styles.dueDateItemCheckboxWrap}>
+                  <span 
+                    className={`${styles.dueDateItemCheckbox} ${checkbox ? styles.checked : "" }`}
+                    onClick={onDeteWindow}
+                  >
+                    <Icons 
+                      name={'selected_label'}
+                      class_name={'iconSelectedLabel'}
+                      fill={"#1d2125"}
+                    />
+                  </span>
+                </span>
+              </label>
+              <input 
+                className={styles.dueDateTextInput} 
+                type="text" placeholder="Д.М.ГГГГ" 
+                aria-placeholder="Д.М.ГГГГ" 
+                name="date" 
+                value={checkbox ? takeDate() : ""}  
+                disabled={checkbox ? "" : true}
+              />
+            </div>
+          </DatePicker>
         </div>
 
-        <label className={styles.dueDateItem}>
-          <input className={styles.dueDateItemInput} type="checkbox" />
-          
-          <span className={styles.dueDateItemCheckboxWrap}>
-            <span 
-              className={`${styles.dueDateItemCheckbox} ${checkbox ? styles.checked : "" }`}
-              onClick={onDeteWindow}
-            >
-              <Icons 
-                name={'selected_label'}
-                class_name={'iconSelectedLabel'}
-                fill={"#1d2125"}
-              />
+        {/* <div className={styles.dueDateItemWrap}>
+          <label className={styles.dueDateItem}>
+            <input className={styles.dueDateItemInput} type="checkbox" />
+            
+            <span className={styles.dueDateItemCheckboxWrap}>
+              <span 
+                className={`${styles.dueDateItemCheckbox} ${checkbox ? styles.checked : "" }`}
+                onClick={onDeteWindow}
+              >
+                <Icons 
+                  name={'selected_label'}
+                  class_name={'iconSelectedLabel'}
+                  fill={"#1d2125"}
+                />
+              </span>
             </span>
-          </span>
-        </label>
-        <input 
-          className={styles.dueDateTextInput} 
-          type="text" placeholder="Д.М.ГГГГ" 
-          aria-placeholder="Д.М.ГГГГ" 
-          name="date" 
-          value={checkbox ? startDate : ""}  
-          disabled={checkbox ? "" : true}
-        />
+          </label>
+          <input 
+            className={styles.dueDateTextInput} 
+            type="text" placeholder="Д.М.ГГГГ" 
+            aria-placeholder="Д.М.ГГГГ" 
+            name="date" 
+            value={checkbox ? startDate : ""}  
+            disabled={checkbox ? "" : true}
+          />
+        </div> */}
 
 
 
@@ -133,7 +173,7 @@ export default function SidebarDueDate(props){
             className={'dueDateCancel'} 
             // actionVariable={null}
             clickAction = {funcEraseDates}
-          >Отмена</Button>
+          >Сброс</Button>
         </div>
 
     </div>
